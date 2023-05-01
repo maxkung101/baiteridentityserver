bb79ea2d0a0bbdb3d580e8dd86c4da327e1b3a40
Max Kung <maxkung06@gmail.com>
2023-05-01 18:38:08 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Baiter Identity Api
+A server program used to generate a false identity for scambaiting
+Runs on Linux
+
+Introduction
+------------
+People in the United States have been receiving unwanted phone calls from scammers who want personal information. This repository gives scambaiters information of a fake person.
+
+Download source code
+--------------------
+Please read the [terms of use](https://github.com/maxkung101/baiteridentityserver/blob/master/terms.txt) before downloading the source code.
+```
+$ gh repo clone maxkung101/baiteridentityserver
+```
+Note: You must use this program only to talk to scammers.
+
+Credits
+-------
+Programmed by Max Kung
---
+++ app.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+from flask import Flask, jsonify
+from werkzeug.middleware.proxy_fix import ProxyFix
+import subprocess, random
+
+app = Flask(__name__)
+app.wsgi_app = ProxyFix(
+        app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1
+)
+
+@app.route('/')
+def hello():
+        #returns a success route
+        return jsonify({'errorCode' : 'none', 'title' : 'Hello', 'message' : 'Success'})
+
+@app.route('/baiterIdentity')
+def baiterIdentity():
+        #returns a fake identity
+        try:
+                output = subprocess.check_output(['rig'])
+                output_str = output.decode('utf-8')
+                return jsonify({'errorCode' : 'none', 'title' : 'Fake identity successful', 'message' : output_str})
+        except subprocess.CalledProcessError as e:
+                return jsonify({'errorCode' : 'Called Process Error', 'title' : 'Fake identity failed', 'message' : e.output.decode('utf-8')}), e.returncode
+
+@app.route('/creditcard')
+def creditcard():
+        #returns a fake card number
+        heading = random.randint(2, 5)
+        message = str(heading)
+        title = ""
+        if heading == 3:
+                title = title + "American Express"
+                message = message + str(random.randint(0, 9)) + str(random.randint(0, 9))
+        elif heading == 4:
+                title = title + "Visa"
+                message = message + str(random.randint(0, 9)) + str(random.randint(0, 9)) + str(random.randint(0, 9))
+        else:
+                title = title + "Mastercard"
+                message = message + str(random.randint(0, 9)) + str(random.randint(0, 9)) + str(random.randint(0, 9))
+        message = message + " " + str(random.randint(0, 9)) + str(random.randint(0, 9)) + str(random.randint(0, 9)) + str(random.randint(0, 9)) + " " + str(random.randint(0, 9)) + str(random.randint(0, 9)) + str(random.randint(0, 9)) + str(random.randint(0, 9)) + " " + str(random.randint(0, 9)) + str(random.randint(0, 9)) + str(random.randint(0, 9)) + str(random.randint(0, 9))
+        try:
+                return jsonify({'errorCode' : 'none', 'title' : title, 'message' : message})
+        except:
+                return jsonify({'errorCode' : 'Other', 'title' : 'Fake card failed', 'message' : 'error'})
+
+@app.errorhandler(404)
+def invalid_route(e):
+        #returns an invalid route
+        return jsonify({'errorCode' : 404, 'title' : 'Error code 404', 'message' : 'Route not found'})
+
+if __name__ == '__main__':
+        app.run(port=8080, debug=True)
